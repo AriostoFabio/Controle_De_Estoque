b6011c3dc86f47d922c255e4ced2242c9c4b39ed
Fabio <102165904+AriostoFabio@users.noreply.github.com>
2023-11-24 10:57:58 -0300

    Update estoque.py
    atualização de código

--- estoque.py
+++ estoque.py
@@ -1,3 +1,11 @@
+'''
+- criar uma função que deixe os items abaixo de uma determinada quantidade em vermelho
+- possibilidade de essa quantidade minima ser editada
+- criar um filtro para mostrar quantidades acima e abaixo de um numero x
+
+-arrumar a função ver tabela, fazer com que a tabela sempre seja mostrada, e atualizada a cada função executada
+
+'''
 import tkinter as tk
 from tkinter import simpledialog, messagebox
 import sqlite3
@@ -8,7 +16,7 @@ from datetime import date
 conn = sqlite3.connect('DB_ESTOQUE.db')
 c = conn.cursor()
 c.execute('''CREATE TABLE if not exists estoque
-             (id INTEGER PRIMARY KEY, nome text, quantidade text) ''')
+             (id INTEGER PRIMARY KEY, nome text, quantidade text, data text) ''')
 
 # Interface e funções
 root = tk.Tk()
@@ -19,14 +27,14 @@ root.option_add("*Font", "Arial 12")
 #cria uma data
 def data_atual():
     x = datetime.datetime.now()
-    return x.strftime("%d/%m/%Y %H:%M")
-
+    return str(x.strftime("%d/%m/%Y %H:%M"))
+    
 def vertabela():
     resultado = c.execute('SELECT * FROM estoque')
     text_output.delete(1.0, tk.END)
     for item in resultado.fetchall():
         # Converte o item[1] (nome do produto) para maiúsculas
-        line = "|{:<5}|{:<30}|{:<5}|{:<1}".format(item[0], item[1].upper(), item[2], item[3])
+        line = "|{:<2}|{:<25}|{:<3}|{:<5}".format(item[0], item[1].upper(), item[2], item[3])
         text_output.insert(tk.END, line + '\n')
 
 def inserirvalor():
@@ -107,12 +115,11 @@ btn_alterar = tk.Button(menu_frame, text="Alterar", command=alterar, width=15, h
 btn_alterar.grid(row=0, column=2, padx=5)
 
 btn_deletar = tk.Button(menu_frame, text="Deletar", command=deletar, width=15, height=2)
-btn_deletar.grid(row=0, column=2, padx=5)
+btn_deletar.grid(row=0, column=3, padx=5)
 
 btn_sair = tk.Button(menu_frame, text="Sair", command=sair, width=15, height=2)
-btn_sair.grid(row=0, column=3, padx=5)
+btn_sair.grid(row=0, column=4, padx=5)
 
-text_output = tk.Text(root, width=50, height=20)
 text_output = tk.Text(root, width=60, height=20, font=("Courier New", 12))
 text_output.pack(pady=3, padx=15)
 
